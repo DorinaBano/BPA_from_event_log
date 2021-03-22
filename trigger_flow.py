033fca9b45042e7f389e557961c46d4361ab2fc7
dorina1985 <60092221+dorina1985@users.noreply.github.com>
2021-03-22 10:13:31 +0100

    Update trigger_flow.py

--- trigger_flow.py
+++ trigger_flow.py
@@ -1,28 +1,23 @@
 from pm4py.objects.log.importer.xes import importer as xes_importer
 import sys
-log = xes_importer.apply('Pl_2020_role.xes')
+log = xes_importer.apply('BPI_2017_all.xes')
 
 for case_index, case in enumerate(log):
     event_list=list(enumerate(case))
     old_event_index = -1
     for event_index, event in enumerate(case):
-        if event["concept:name"] == "S":
+        if event["concept:name"] == "O_Create Offer":
             for i in range(1,event_index):
                 prev_index = event_index-i
                 complete_event = event_list[prev_index]
                 catchedEvent = complete_event[1]
-                if catchedEvent["concept:name"].startswith("2_"):
+                if not catchedEvent["concept:name"].startswith("O_"):
                     if prev_index == old_event_index:
                         break
                     old_event_index=prev_index
-                    file = open('trigger_text.csv', 'a')
+                    file = open('trigger_flow_aux.csv', 'a')
                     sys.stdout = file
                     print(case.attributes["concept:name"], ",",catchedEvent["time:timestamp"], ",",catchedEvent["concept:name"])
                     print(case.attributes["concept:name"], ",",event["time:timestamp"], ",",event["concept:name"])
                     break
                     file.close()
-counter_list = list(enumerate(log))
-
-
-
-
